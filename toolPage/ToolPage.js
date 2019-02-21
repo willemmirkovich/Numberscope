95fc1f7193ce33abd2423fc23a6a2ca6cb2d8674
Josiah Martinez <joma7909@colorado.edu>
2019-02-21 09:54:38 -0700

    variable name changes

--- toolPage/ToolPage.js
+++ toolPage/ToolPage.js
@@ -12,10 +12,10 @@ function openExNav() {
     document.getElementById("title1").style.marginLeft = "4.5em";
     document.getElementById("mainCanvas").style.marginLeft = "12em";
 
-    var list1 = ["sideNav", "stepNav", "selectNav", "seqInputNav"];
-    for (var i = 0; i < list1.length; i++) {
-      var elements = document.getElementsByClassName(list1[i]);
-      for(var j = 0, length = elements.length; j < length; j++) {
+    let list1 = ["sideNav", "stepNav", "selectNav", "seqInputNav"];
+    for (let i = 0; i < list1.length; i++) {
+      let elements = document.getElementsByClassName(list1[i]);
+      for(let j = 0, length = elements.length; j < length; j++) {
         elements[j].style.marginLeft = "12em";
       }
     }
@@ -25,10 +25,10 @@ function closeExNav() {
     document.getElementById("title1").style.marginLeft = "0";
     document.getElementById("mainCanvas").style.marginLeft = "0";
 
-    var list1 = ["sideNav", "stepNav", "selectNav", "seqInputNav"];
-    for (var i = 0; i < list1.length; i++) {
-      var elements = document.getElementsByClassName(list1[i]);
-      for(var j = 0, length = elements.length; j < length; j++) {
+    let list1 = ["sideNav", "stepNav", "selectNav", "seqInputNav"];
+    for (let i = 0; i < list1.length; i++) {
+      let elements = document.getElementsByClassName(list1[i]);
+      for(let j = 0, length = elements.length; j < length; j++) {
         elements[j].style.marginLeft = "0em";
       }
     }
@@ -87,15 +87,15 @@ function openCanvas(){
 //Sequence Navigation Bars
 function openSeqNav(n){
   document.getElementById("title1").innerHTML = "Pick an Input Method";
-  for (var i = 1; i <= numSequences; i++) {
-    var curSeqNav = "seqNav" + i;
+  for ( i = 1; i <= numSequences; i++) {
+    let curSeqNav = "seqNav" + i;
     document.getElementById(curSeqNav).style.width = "0em";
   }
-  var selectedNav = "seqNav" + n;
+  let selectedNav = "seqNav" + n;
   document.getElementById(selectedNav).style.width = "12em";
 
-  for (var i = 1; i <= numSequences; i++) {
-    var curSeq = "seq" + i;
+  for (let i = 1; i <= numSequences; i++) {
+    let curSeq = "seq" + i;
     if (i == n) {
       document.getElementById(curSeq).style.background = logoColor;
     }
@@ -104,17 +104,17 @@ function openSeqNav(n){
     }
   }
 
-  for (var i = 1; i <= numSequences; i++) {
-    for (var j = 1; j <= 4; j++) {
+  for (let i = 1; i <= numSequences; i++) {
+    for (let j = 1; j <= 4; j++) {
       closeSeqInputNav(i,j);
     }
   }
 }
 function closeSeqNav(n){
-  var selectedNav = "seqNav" + n;
+  let selectedNav = "seqNav" + n;
   document.getElementById(selectedNav).style.width = "0";
   for (let i = 1; i <= numSequences; i++) {
-    var curSeq = "seq" + i;
+    let curSeq = "seq" + i;
     document.getElementById(curSeq).style.background = sideNavColor2;
   }
   for (let i = 1; i <= numSequences; i++) {
@@ -126,47 +126,47 @@ function closeSeqNav(n){
 
 function openSeqInputNav(n,m){
   if (m==1) {
-    let f = "builtInInputNav" + n;
-    document.getElementById(f).style.width = "24em";
-    let g = "builtIn" + n;
-    document.getElementById(g).style.background = topBarColor;
+    let u = "builtInInputNav" + n;
+    document.getElementById(u).style.width = "24em";
+    let v = "builtIn" + n;
+    document.getElementById(v).style.background = topBarColor;
 
-    var linRecNumber = "builtInSelect" + n;
-    var linRecNav = "linRecNav" + n;
-    if (document.getElementById(linRecNumber).value == "linRec") {
-      extendLinRec(n);
-    }
+    // let linRecNumber = "builtInSelect" + n;
+    // let linRecNav = "linRecNav" + n;
+    // if (document.getElementById(linRecNumber).value == "linRec") {
+    //   extendLinRec(n);
+    // }
 
-    closeSeqInputNav(n,m=2);
-    closeSeqInputNav(n,m=3);
-    closeSeqInputNav(n,m=4);
+    closeSeqInputNav(n,3);
+    closeSeqInputNav(n,4);
+    closeSeqInputNav(n,2);
   }
   else if (m==2) {
-    let f = "oeisInputNav" + n;
-    document.getElementById(f).style.width = "12em";
-    let g = "OEIS" + n;
-    document.getElementById(g).style.background = topBarColor;
-    closeSeqInputNav(n,m=1);
-    closeSeqInputNav(n,m=3);
-    closeSeqInputNav(n,m=4);
+    let u = "oeisInputNav" + n;
+    document.getElementById(u).style.width = "12em";
+    let v = "OEIS" + n;
+    document.getElementById(v).style.background = topBarColor;
+    closeSeqInputNav(n,4);
+    closeSeqInputNav(n,3);
+    closeSeqInputNav(n,1);
   }
   else if (m==3) {
-    let f = "listInputNav" + n;
-    document.getElementById(f).style.width = "24em";
-    let g = "list" + n
-    document.getElementById(g).style.background = topBarColor;
-    closeSeqInputNav(n,m=1);
-    closeSeqInputNav(n,m=2);
-    closeSeqInputNav(n,m=4);
+    let u = "listInputNav" + n;
+    document.getElementById(u).style.width = "24em";
+    let v = "list" + n
+    document.getElementById(v).style.background = topBarColor;
+    closeSeqInputNav(n,4);
+    closeSeqInputNav(n,2);
+    closeSeqInputNav(n,1);
   }
   else if (m==4) {
-    let f = "codeInputNav" + n;
-    document.getElementById(f).style.width = "24em";
-    let g = "code" + n;
-    document.getElementById(g).style.background = topBarColor;
-    closeSeqInputNav(n,m=1);
-    closeSeqInputNav(n,m=2);
-    closeSeqInputNav(n,m=3);
+    let u = "codeInputNav" + n;
+    document.getElementById(u).style.width = "24em";
+    let v = "code" + n;
+    document.getElementById(v).style.background = topBarColor;
+    closeSeqInputNav(n,3);
+    closeSeqInputNav(n,2);
+    closeSeqInputNav(n,1);
   }
 }
 function closeSeqInputNav(n,m){
@@ -175,7 +175,7 @@ function closeSeqInputNav(n,m){
     document.getElementById(f).style.width = "0";
     let g = "builtIn" + n;
     document.getElementById(g).style.background = logoColor;
-    var linRecNav = "linRecNav" + n;
+    let linRecNav = "linRecNav" + n;
     document.getElementById(linRecNav).style.height = "0em";
   }
   else if (m==2) {
@@ -202,15 +202,15 @@ function openToolInputNav(n,m){
 
 }
 //Linear Recurrence
-var linRecHeights = ["8.5em", "11.5em", "14em", "16.5em", "19.5em", "22em", "24.7em", "27.5em", "30em", "34em" ]
+let linRecHeights = ["8.5em", "11.5em", "14em", "16.5em", "19.5em", "22em", "24.7em", "27.5em", "30em", "34em" ]
 function openLinRec(n){
-  var linRecNumber = "builtInSelect" + n;
-  var linRecNav = "linRecNav" + n;
+  let linRecNumber = "builtInSelect" + n;
+  let linRecNav = "linRecNav" + n;
   if (document.getElementById(linRecNumber).value == "linRec") {
-    var curkSelect = "kSelect" + n;
-    var linRecNavLength = document.getElementById(curkSelect).value;
-    var linRecNavLength = parseInt(linRecNavLength, 10) - 1;
-    var newHeight = linRecHeights[linRecNavLength];
+    let curkSelect = "kSelect" + n;
+    let linRecNavLength = document.getElementById(curkSelect).value;
+    let linRecNavLength = parseInt(linRecNavLength, 10) - 1;
+    let newHeight = linRecHeights[linRecNavLength];
     document.getElementById(linRecNav).style.height = newHeight;
   }
   else {
@@ -218,19 +218,19 @@ function openLinRec(n){
   }
 }
 function extendLinRec(n){
-  var linRecNav = "linRecNav" + n;
-  var curkSelect = "kSelect" + n;
-  var linRecNavLength = document.getElementById(curkSelect).value;
-  var linRecNavLength = parseInt(linRecNavLength, 10) - 1;
-  var newHeight = linRecHeights[linRecNavLength];
+  let linRecNav = "linRecNav" + n;
+  let curkSelect = "kSelect" + n;
+  let linRecNavLength = document.getElementById(curkSelect).value;
+  let linRecNavLength = parseInt(linRecNavLength, 10) - 1;
+  let newHeight = linRecHeights[linRecNavLength];
   document.getElementById(linRecNav).style.height = newHeight;
 }
 
 //Tool Navigation Bars
 function openToolNav(n){
   document.getElementById("toolNav").style.width = "12em";
-  for (var i = 1; i <= numTools; i++) {
-    var curTool = "tool" + i;
+  for (let i = 1; i <= numTools; i++) {
+    let curTool = "tool" + i;
     if (i == n) {
       document.getElementById(curTool).style.background = logoColor;
     }
@@ -243,8 +243,8 @@ function closeToolNav(n){
   if (n==1) {
     //document.getElementById("toolNav").style.marginLeft = "-12em";
     document.getElementById("toolNav").style.width = "0";
-    for (var i = 1; i <= numTools; i++) {
-      var curTool = "tool" + i;
+    for (let i = 1; i <= numTools; i++) {
+      let curTool = "tool" + i;
       document.getElementById(curTool).style.background = sideNavColor2;
     }
   }
@@ -252,52 +252,52 @@ function closeToolNav(n){
 
 function addSeq(){
   if(numSequences < 10){
-    var seqLetter = String.fromCharCode(97 + numSequences);
+    let seqLetter = String.fromCharCode(97 + numSequences);
     numSequences += 1;
 
-    var a = document.createElement('a');
+    let a = document.createElement('a');
     a.innerHTML = "{" + seqLetter + "<sub>n</sub>}";
     a.setAttribute("href", "#");
 
-    var idName = "seq" + numSequences;
+    let idName = "seq" + numSequences;
     a.setAttribute("id", idName);
 
-    var fctCall = "openSeqNav(n=" + numSequences + ")";
+    let fctCall = "openSeqNav(n=" + numSequences + ")";
     a.setAttribute("onclick", fctCall);
 
     document.getElementById("step1Nav").append(a);
     ////////////////////////////////
-    for (var i = 1; i <= numDraw; i++) {
-      var opt = document.createElement("option");
+    for (let i = 1; i <= numDraw; i++) {
+      let opt = document.createElement("option");
       opt.innerHTML = "{" + seqLetter + "<sub>n</sub>}";
 
-      var valueName = numSequences;
+      let valueName = numSequences;
       opt.setAttribute("value", valueName);
 
-      var curSelect = "selectSeq" + i;
+      let curSelect = "selectSeq" + i;
       document.getElementById(curSelect).append(opt);
     }
     ////////////////////////////////
     //Create new Function Input Nav
-    var newNav = document.createElement("div");
-    var navId = "seqNav" + numSequences;
+    let newNav = document.createElement("div");
+    let navId = "seqNav" + numSequences;
     newNav.setAttribute("id", navId);
     newNav.setAttribute("class", "selectNav");
 
     //Close Btn Link
-    var a = document.createElement('a');
-    var newId = "seqCloseBtn" + numSequences;
+    a = document.createElement('a');
+    let newId = "seqCloseBtn" + numSequences;
     a.setAttribute("id", newId);
     a.setAttribute("href", "javascript:void(0)");
     a.setAttribute("class", "selectClosebtn");
-    var fctCall = "closeSeqNav(n=" + numSequences + ")";
+    fctCall = "closeSeqNav(n=" + numSequences + ")";
     a.setAttribute("onclick", fctCall);
     a.innerHTML = "&#171";
     newNav.appendChild(a);
 
     //builtIn Link
-    var builtInLink = document.createElement('a');
-    var builtInId = "builtIn" + numSequences;
+    let builtInLink = document.createElement('a');
+    let builtInId = "builtIn" + numSequences;
     builtInLink.setAttribute("id", builtInId);
     builtInLink.setAttribute("href", "#");
     fctCall = "openSeqInputNav(n=" + numSequences + ",m=1)";
@@ -306,8 +306,8 @@ function addSeq(){
     newNav.appendChild(builtInLink);
 
     //OEIS Link
-    var oeisLink = document.createElement('a');
-    var oeisId = "OEIS" + numSequences;
+    let oeisLink = document.createElement('a');
+    let oeisId = "OEIS" + numSequences;
     oeisLink.setAttribute("id", oeisId);
     oeisLink.setAttribute("href", "#");
     fctCall = "openSeqInputNav(n=" + numSequences + ",m=2)";
@@ -316,8 +316,8 @@ function addSeq(){
     newNav.appendChild(oeisLink);
 
     //List Link
-    var listLink = document.createElement('a');
-    var listId = "list" + numSequences;
+    let listLink = document.createElement('a');
+    let listId = "list" + numSequences;
     listLink.setAttribute("id", listId);
     listLink.setAttribute("href", "#");
     fctCall = "openSeqInputNav(n=" + numSequences + ",m=3)";
@@ -326,8 +326,8 @@ function addSeq(){
     newNav.appendChild(listLink);
 
     //Code Link
-    var codeLink = document.createElement('a');
-    var codeId = "code" + numSequences;
+    let codeLink = document.createElement('a');
+    let codeId = "code" + numSequences;
     codeLink.setAttribute("id", codeId);
     codeLink.setAttribute("href", "#");
     fctCall = "openSeqInputNav(n=" + numSequences + ",m=4)";
@@ -338,13 +338,13 @@ function addSeq(){
     document.getElementById("seqNavs").append(newNav);
     ///////////////////////////////////////////////
     //Create Function Input Nav
-    var newNav = document.createElement("div");
+    newNav = document.createElement("div");
     newId = "builtInInputNav" + numSequences;
     newNav.setAttribute("id", newId);
     newNav.setAttribute("class", "seqInputNav");
 
-    var a = document.createElement('a');
-    var closeId = "builtInCloseBtn" + numSequences;
+    a = document.createElement('a');
+    let closeId = "builtInCloseBtn" + numSequences;
     a.setAttribute("id", closeId);
     a.setAttribute("href", "javascript:void(0)");
     a.setAttribute("class", "closebtn");
@@ -356,13 +356,13 @@ function addSeq(){
     document.getElementById("builtInInputNavs").append(newNav);
 
     //Create OEIS Input Nav
-    var newNav = document.createElement("div");
+    newNav = document.createElement("div");
     newId = "oeisInputNav" + numSequences;
     newNav.setAttribute("id", newId);
     newNav.setAttribute("class", "seqInputNav");
 
-    var a = document.createElement('a');
-    var closeId = "oeisCloseBtn" + numSequences;
+    a = document.createElement('a');
+    closeId = "oeisCloseBtn" + numSequences;
     a.setAttribute("id", closeId);
     a.setAttribute("href", "javascript:void(0)");
     a.setAttribute("class", "closebtn");
@@ -371,11 +371,11 @@ function addSeq(){
     a.innerHTML = "&#171";
     newNav.appendChild(a);
 
-    var newli = document.createElement("li");
+    newli = document.createElement("li");
     newli.innerHTML = "OEIS Number: ";
     newNav.appendChild(newli);
 
-    var newText = document.createElement("textarea");
+    newText = document.createElement("textarea");
     newId = "oeisNum" + numSequences;
     newText.setAttribute("id", newId);
     newText.setAttribute("class", "oeisNum");
@@ -384,13 +384,13 @@ function addSeq(){
     document.getElementById("oeisInputNavs").append(newNav);
 
     //Create List Input Nav
-    var newNav = document.createElement("div");
+    newNav = document.createElement("div");
     newId = "listInputNav" + numSequences;
     newNav.setAttribute("id", newId);
     newNav.setAttribute("class", "seqInputNav");
 
-    var a = document.createElement('a');
-    var closeId = "listCloseBtn" + numSequences;
+    a = document.createElement('a');
+    closeId = "listCloseBtn" + numSequences;
     a.setAttribute("id", closeId);
     a.setAttribute("href", "javascript:void(0)");
     a.setAttribute("class", "closebtn");
@@ -402,13 +402,13 @@ function addSeq(){
     document.getElementById("listInputNavs").append(newNav);
 
     //Create Code Input Nav
-    var newNav = document.createElement("div");
+    newNav = document.createElement("div");
     newId = "codeInputNav" + numSequences;
     newNav.setAttribute("id", newId);
     newNav.setAttribute("class", "seqInputNav");
 
-    var a = document.createElement('a');
-    var closeId = "codeCloseBtn" + numSequences;
+    a = document.createElement('a');
+    closeId = "codeCloseBtn" + numSequences;
     a.setAttribute("id", closeId);
     a.setAttribute("href", "javascript:void(0)");
     a.setAttribute("class", "closebtn");
@@ -422,29 +422,29 @@ function addSeq(){
 }
 function addTool(){
   if(numTools < 10){
-    var toolNum = numTools + 1;
+    let toolNum = numTools + 1;
     numTools += 1;
 
-    var a = document.createElement('a');
+    let a = document.createElement('a');
     a.innerHTML = "Tool " + toolNum;
     a.setAttribute("href", "#");
 
-    var idName = "tool" + numTools;
+    let idName = "tool" + numTools;
     a.setAttribute("id", idName);
 
-    var fctCall = "openToolNav(n=" + numTools + ")";
+    let fctCall = "openToolNav(n=" + numTools + ")";
     a.setAttribute("onclick", fctCall);
 
     document.getElementById("step2Nav").append(a);
     ////////////////////////////////
-    for (var i = 1; i <= numDraw; i++) {
-      var opt = document.createElement("option");
+    for (let i = 1; i <= numDraw; i++) {
+      let opt = document.createElement("option");
       opt.innerHTML = "Tool " + numTools;
 
-      var valueName = numTools;
+      let valueName = numTools;
       opt.setAttribute("value", valueName);
 
-      var curSelect = "selectTool" + i;
+      let curSelect = "selectTool" + i;
       document.getElementById(curSelect).append(opt);
     }
   }
@@ -453,46 +453,46 @@ function addDraw(){
   if(numDraw < 10){
     numDraw += 1;
     /////////////////////////////////////
-    var a = document.createElement('a');
+    let a = document.createElement('a');
     a.innerHTML = numDraw + ".";
     a.setAttribute("href", "#");
     a.setAttribute("class", "numColumn");
     document.getElementById("drawListNum").append(a);
     /////////////////////////////////////
-    var sel = document.createElement("select");
-    var selIDName = "selectSeq" + numDraw;
+    let sel = document.createElement("select");
+    let selIDName = "selectSeq" + numDraw;
     sel.setAttribute("id", selIDName);
     document.getElementById("drawSeqList").append(sel);
 
-    for (var i = 1; i <= numSequences; i++) {
-      var j = 97 + (i - 1);
-      var seqLetter = String.fromCharCode(j);
-      var opt = document.createElement("option");
+    for (let i = 1; i <= numSequences; i++) {
+      let j = 97 + (i - 1);
+      let seqLetter = String.fromCharCode(j);
+      let opt = document.createElement("option");
       opt.innerHTML = "{" + seqLetter + "<sub>n</sub>}";
 
-      var valueName = "s" + i;
+      let valueName = "s" + i;
       opt.setAttribute("value", valueName);
 
       document.getElementById(selIDName).append(opt);
     }
     /////////////////////////////////////
-    var sel = document.createElement("select");
-    var selIDName = "selectTool" + numDraw;
+    let sel = document.createElement("select");
+    let selIDName = "selectTool" + numDraw;
     sel.setAttribute("id", selIDName);
     document.getElementById("drawToolList").append(sel);
 
-    for (var i = 1; i <= numTools; i++) {
-      var seqLetter = String.fromCharCode(j);
-      var opt = document.createElement("option");
+    for (let i = 1; i <= numTools; i++) {
+      let seqLetter = String.fromCharCode(j);
+      let opt = document.createElement("option");
       opt.innerHTML = "Tool " + i;
 
-      var valueName = "t" + i;
+      let valueName = "t" + i;
       opt.setAttribute("value", valueName);
 
       document.getElementById(selIDName).append(opt);
     }
     // /////////////////////////////////////
-    var a = document.createElement('a');
+    let a = document.createElement('a');
     a.innerHTML = "&times";
     a.setAttribute("href", "#");
     a.setAttribute("class", "xColumn");
