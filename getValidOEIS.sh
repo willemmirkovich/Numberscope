a0d6bf0e3dc24a29cfbf3810ff9afa23902ea7c0
Abdulatif <AbKhAbAj@gmail.com>
2019-11-25 13:11:17 -0700

    script to get OEIS seqs defined in sage

---
+++ getValidOEIS.sh
@@ -0,0 +1,8 @@
+
+
+if [ ! -f javascript/core/validOEIS.js ]; 
+then 
+    curl https://raw.githubusercontent.com/sagemath/sagelib/master/sage/combinat/sloane_functions.py |
+    grep -Po "^class \K(A\d+)" |
+    python3 -c "import sys,json; print('export const validOEIS = ' + json.dumps(sys.stdin.read().split('\n')[:-1]))" > "javascript/core/validOEIS.js"
+fi
