c868a4404dcfebaa7a5ddd8bc37513ad6437abbd
Abdullatif <abal2051@colorado.edu>
2019-12-02 06:09:02 -0700

    Move TA-Visualizers to misc

--- misc/TA-Visualizers/Spirals/sketch.js
+++ misc/TA-Visualizers/Spirals/sketch.js
@@ -1,26 +1,40 @@
 function setup() {
-	createCanvas(640,640);
+	createCanvas(640, 640);
 	fill(0);
-	setFrameRate(10);
+	setFrameRate(5);
+	Index = 0;
+	backGroundColor = color(0, 25, 0);
+	initialColor = color(50, 255, 0);
+	finalColor = color(0, 170, 0);
+	transitionFrames = 10.0;
+	myList = makeHexList(20);
+	fill(backGroundColor);
+	for (let a = 0; a < myList.length; a++) {
+		coord = new coordinate(300 + 10 * myList[a].x, 250 + 10 * myList[a].y);
+		polygon(coord.x, coord.y, 6, 6);
+	}
 }
 
-function draw() {	
-    myList=makeTriList(25);
-	for (a=0; a<myList.length; a++)
-	{
-		if (a==0)
-		{
-			fill(0);
+
+function draw() {
+	theList = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987];
+	for (let b = 0; b < theList.length; b++) {
+		if (b <= Index) {
+			let framesSince = Index - b;
+			let inter = (transitionFrames - framesSince) / transitionFrames;
+			let theColor = lerpColor(finalColor, initialColor, inter)
+			fill(theColor);
+		} else {
+			fill(backGroundColor);
 		}
-		else if (onSequence(a+1))
-		{
-			fill(255,0,0);
-		}
-		else
-		{
-			fill (0,255,0);
-		}
-		coord=new coordinate(300+10*myList[a].x,250+10*myList[a].y);
-		ellipse(coord.x,coord.y,12,12);
+		coord = new coordinate(300 + 10 * myList[theList[b]].x, 250 + 10 * myList[theList[b]].y);
+		polygon(coord.x, coord.y, 6, 6);
 	}
+
+	Index++;
+	if (Index == myList.length) {
+		noLoop();
+	}
+
+
 }
