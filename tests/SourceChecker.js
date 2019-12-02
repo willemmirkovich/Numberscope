a0342be59bc12548654b098fd484d278061322ba
Abdullatif <abal2051@colorado.edu>
2019-12-01 18:07:09 -0700

    Added basic tests for module

---
+++ tests/SourceChecker.js
@@ -0,0 +1,74 @@
+var ls = require('lexical-scope');
+var acorn = require('acorn');
+var p5 = require('p5');
+
+function checkSourceP5(source) {
+    let except = ['draw', 'setup', "undefined", "null"];
+    let hits = [];
+    let res = ls(source)
+    res.globals.implicit.forEach(ref => {
+        if (p5.prototype.hasOwnProperty(ref) && !except.includes(ref)) {
+            hits.push({
+                'is': ref,
+                'shouldBe': 'this.sketch.' + ref,
+                loc: null
+            })
+            if( Math.hasOwnProperty(ref) ){
+                hits[ hits.length - 1].shouldBe = hits[ hits.length - 1].shouldBe + " (or Math." + hits[ hits.length - 1].is +" instead)";
+            }
+        }
+    });
+
+    let tokens = [...acorn.tokenizer(source, {
+        'locations': true
+    })]
+    tokens.forEach(function (token, index) {
+        let foundIndex = hits.findIndex(elm => elm.is == token.value)
+        if (foundIndex != -1) {
+            hits[foundIndex].loc = token.loc
+        }
+    })
+    return hits
+}
+
+function checkSourceSeq(source){
+    let hits = [];
+    let tokens = [...acorn.tokenizer(source, {
+        'locations': true
+    })]
+    let refsToSeq = ['seq']
+    console.log(tokens)
+    tokens.forEach(function (token, index) {
+        if (token.value != undefined && index - 3 >= 0) {
+            let lookingAt = token.value;
+            let assignIndexOffset = -1;
+            if (tokens[index - 1].type.label == '.') {
+                lookingAt = tokens[index - 2].value + tokens[index - 1].type.label + tokens[index].value;
+                assignIndexOffset = -3;
+            }
+            let assignIndex = index + assignIndexOffset
+            if (!refsToSeq.includes(lookingAt)) {
+                return
+            } else {}
+            if (tokens[assignIndex].value == "=") {
+                if (tokens[assignIndex - 2].type.label == '.') {
+                    newRef = tokens[assignIndex - 3].value + tokens[assignIndex - 2].type.label + tokens[assignIndex - 1].value
+                } else {
+                    newRef = tokens[assignIndex - 1].value
+                }
+                refsToSeq.push(newRef);
+            }
+            if (tokens[index + 1].type.label == "[") {
+                hits.push({
+                    'is': lookingAt + '[' + tokens[index + 2].value + ']',
+                    'shouldBe': lookingAt + ".getElement(" + tokens[index + 2].value + ")",
+                    loc: token.loc
+                })
+            }
+        }
+    })
+    return hits;
+}
+
+window.checkSourceP5 = checkSourceP5;
+window.checkSourceSeq = checkSourceSeq;
---
+++ tests/test.js
@@ -0,0 +1,50 @@
+let expect = chai.expect;
+
+describe( "Modules", function(){
+
+    Object.keys(NScore.modules).forEach( function(mKey){
+
+        const m = NScore.modules[mKey];
+
+        describe( `${mKey}`, function(){
+
+                it('has a name', function(done){
+                    chai.expect( m ).to.have.property("name");
+                    chai.expect( m.name ).to.be.a("string");
+                    done();
+                })
+
+                it('has a schema', function(done){
+                    chai.expect( m ).to.have.property("configSchema");
+                    chai.expect( m.configSchema ).to.be.a("Object");
+                    done();
+                })
+
+                it('has a viz class that is properly structured', function(done){
+                    chai.expect( m ).to.have.property("viz");
+                    chai.expect( m.viz ).to.be.a("function");
+                    chai.expect( m.viz.prototype ).to.have.property( 'draw' )
+                    chai.expect( m.viz.prototype ).to.have.property( 'setup' )
+                    chai.expect( m.viz.prototype ).to.have.property( 'constructor' )
+                    chai.expect( m.viz.prototype.constructor, 'incorrect number of arguments' ).to.have.length(3)
+                    done();
+                })
+                
+                it('correctly uses this.sketch instead of global p5', function(done){
+                    let hits =  checkSourceP5( m.viz.toString() );
+                    hits.forEach(function(hit){
+                        chai.expect( hit.is, `[${hit.loc.start.line} lines below class definition]` ).to.equal( hit.shouldBe );
+                    })
+                    done()
+                })
+
+                it('correctly uses getElement method instead of []', function(done){
+                    let hits = checkSourceSeq( m.viz.toString() );
+                    hits.forEach(function(hit){
+                        chai.expect( hit.is, `[${hit.loc.start.line} lines below class definition]` ).to.equal( hit.shouldBe );
+                    })
+                    done()
+                })
+        })
+    })
+})
