af74b6a1ce3fe9ef46683df465d367bc83b1f6d0
tlincke125 <thli2739@colorado.edu>
2020-01-13 17:46:13 -0700

    reading through code

---
+++ website/javascript/js/Validation.js
@@ -0,0 +1,12 @@
+/**
+ * @class       : Validation
+ * @author      : theo (theo.j.lincke@gmail.com)
+ * @created     : Monday Jan 13, 2020 17:38:33 MST
+ * @description : Validation
+ */
+
+export default class Validation {
+}
+
+
+class Error {
